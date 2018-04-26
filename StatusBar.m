0cc3e489bf7d84baa6be320da5e4b97da2d6161e
alldone <feiyv666@163.com>
2018-04-26 15:52:02 +0800

    设置Status的字体颜色

---
+++ StatusBar.m
@@ -0,0 +1,20 @@
+//
+//  NavSet.m
+//  iFeieyv
+//
+//  Created by iFeieyv on 2018/4/26.
+//  Copyright © 2018年 Shandong All-Line Technology Co., Ltd. All rights reserved.
+//  用户模型
+
+#import "StatusBar.h"
+
+@implementation StatusBar
+
+//设置为白字体
+[UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+
+//设置为黑字体
+[UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
+
+
+@end
